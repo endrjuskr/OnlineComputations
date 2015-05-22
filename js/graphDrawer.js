ba95fb3d48e8badab9c4abb130841257f58e5e22
Andrzej Skrodzki <andrzej.skrodzki@gmail.com>
2015-05-22 13:46:25 +0200

    Add graph library.

---
+++ js/graphDrawer.js
@@ -0,0 +1,46 @@
+function draw(json, graphElement) {
+    var cy = cytoscape({
+        container: graphElement,
+        style: [
+            {
+                selector: 'node',
+                css: {
+                    'content': 'data(id)',
+                    'text-valign': 'center',
+                    'text-halign': 'center'
+                }
+            },
+            {
+                selector: '$node > node',
+                css: {
+                    'padding-top': '10px',
+                    'padding-left': '10px',
+                    'padding-bottom': '10px',
+                    'padding-right': '10px',
+                    'text-valign': 'top',
+                    'text-halign': 'center'
+                }
+            },
+            {
+                selector: 'edge',
+                css: {
+                    'target-arrow-shape': 'triangle'
+                }
+            },
+            {
+                selector: ':selected',
+                css: {
+                    'background-color': 'black',
+                    'line-color': 'black',
+                    'target-arrow-color': 'black',
+                    'source-arrow-color': 'black'
+                }
+            }
+        ],
+        elements: json,
+        layout: {
+            name: 'cose',
+            padding: 5
+        }
+    });
+}
